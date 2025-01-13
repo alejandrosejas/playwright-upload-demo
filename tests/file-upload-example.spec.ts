8226dfc16cab959ee977a0012f1c028a22385181
Alejandro Sejas <alejandrosejas@outlook.com>
2025-01-13 19:51:31 -0400

    Update file-upload-example.spec.ts

--- tests/file-upload-example.spec.ts
+++ tests/file-upload-example.spec.ts
@@ -19,4 +19,22 @@ test.describe('Upload Files', () => {
     // Assert that the file was uploaded
     await expect(page.locator('#wfu_messageblock_header_1_1')).toContainText('File logo.png uploaded successfully');
   })
+
+  test('Should upload a test file on a hidden input', async ({ page }) => {
+    
+    await page.goto('https://practice.sdetunicorns.com/cart/')
+    const filePath = path.join(__dirname, '../data/logo.png')
+
+    // DOM Manipulation
+    await page.evaluate(()=>{
+      const selector = document.querySelector('input#upfile_1')
+      if(selector){
+        selector.className = ''
+      }
+    })
+
+    await page.setInputFiles('input#upfile_1', filePath)
+    await page.getByRole('button', { name: 'Upload File' }).click()
+    await expect(page.locator('#wfu_messageblock_header_1_1')).toContainText('File logo.png uploaded successfully');
+  })
 });
